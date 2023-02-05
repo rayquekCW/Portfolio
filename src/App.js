8d6dfa2393ddfb7a15cde102779d743c04781b34
Ray Quek <ray.quek@gmail.com>
2023-02-05 16:25:46 +0800

    fix: unuse variable

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Container,Row,Col,Nav, TabContainer, TabContent, TabPane } from "react-bootstrap"
+import { Container,Row,Col} from "react-bootstrap"
 import './App.css';
 import {NavBar} from './components/NavBar';
 import {Banner} from './components/Banner';
